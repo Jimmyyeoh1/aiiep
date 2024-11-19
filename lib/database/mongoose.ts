352616e2c93e58a5a8eb81d8db4b2e34086ecd73
Jimmyyeoh1 <58878001+Jimmyyeoh1@users.noreply.github.com>
2024-11-20 02:02:48 +0800

    implement clerk webhooks

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,34 +1,35 @@
-import mongoose, {Mongoose} from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL= process.env.MONGODB_URL;
+const MONGODB_URL='mongodb+srv://jameshelio:jimmyyeoh@cluster0.oloen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose
 
 if(!cached) {
-    cached = (global as any).mongoose = {conn: null, promise: null}
-
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
- if(cached.conn) return cached.conn;
+  if(cached.conn) return cached.conn;
 
- if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
- cached.promise = 
- cached.promise ||
- mongoose.connect 
-(MONGODB_URL, { dbName:'aiiep', bufferCommands: false}).catch((err) => {
-    console.error('MongoDB connection error:', err);
-    throw new Error('Failed to connect to MongoDB');
-  });
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'aiiep', bufferCommands: false 
+    })
 
-cached.conn = await cached.promise;
 
-return cached.conn;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+
+  
 }
